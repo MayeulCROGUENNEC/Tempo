5aca6078417ef3dcdb86c3c365dcb83000d9a6ca
Mayeul CROGUENNEC <ma.croguennec@orange.fr>
2019-12-07 12:49:37 +0100

    commit V3.3

--- Campagne/js/script.js
+++ Campagne/js/script.js
@@ -3,23 +3,38 @@ new fullpage('#fullpage', {
 });
 
 
-var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 20,
-    stretch: 0,
-    depth: 350,
-    modifier: 1,
-    slideShadows: true
-  },
-  pagination: {
-    el: ".swiper-pagination"
-  }
-});
+var mySwiper = new Swiper ('.swiper-container', {
+  speed: 400,
+  spaceBetween: 100,
+  initialSlide: 0,
+  //truewrapper adoptsheight of active slide
+  autoHeight: false,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // delay between transitions in ms
+  autoplay: 5000,
+  autoplayStopOnLast: false, // loop false also
+  // If we need pagination
+  pagination: '.swiper-pagination',
+  paginationType: "bullets",
 
-$('.killme').click(function(){
-  $(this).parent().remove();
-});
+  // Navigation arrows
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+
+  // And if we need scrollbar
+  //scrollbar: '.swiper-scrollbar',
+  // "slide", "fade", "cube", "coverflow" or "flip"
+  effect: 'slide',
+  // Distance between slides in px.
+  spaceBetween: 60,
+  //
+  slidesPerView: 2,
+  //
+  centeredSlides: true,
+  //
+  slidesOffsetBefore: 0,
+  //
+  grabCursor: true,
+})
